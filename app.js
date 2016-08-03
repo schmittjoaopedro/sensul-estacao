d21e7f51645e698bf93bcf7cd85e71b06a40c068
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2016-08-02 22:08:50 -0300

    v1

---
+++ app.js
@@ -0,0 +1,54 @@
+/**
+* Do the right imports
+*/
+var http = require('http'),
+    httpProxy = require('http-proxy'),
+    express = require('express');
+
+/**
+* Server of node-red.
+* Create the proxy able to redirect HTTP and WebSocket requests.
+* Create an express server to return the static files with the right content type.
+*/
+var proxyApp = httpProxy.createProxyServer({ ws: true }),
+	host = 'http://138.68.21.186:1880',
+	app = express();
+
+app.use(express.static('public'));
+
+proxyApp.on('proxyReq', function(proxyReq, req, res, options) {
+  proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+});
+
+
+var server = http.createServer(app);
+
+/**
+* When the HTTP upgrade is received (WebSocket packet) this packets is redirected to node-red
+*/
+server.on('upgrade', function (req, socket, head) {
+	proxyApp.ws(req, socket, head, {
+			target: host
+		}, receiveError);
+});
+
+/**
+* For all calls for /resource the proxy will redirect to node-red
+*/
+app.use(function(req, res, next) {
+	console.info(req.url);
+	if(req.url.startsWith('/resources')) {
+		proxyApp.web(req, res, {
+			target: host
+		}, receiveError);
+	} else {
+		next();
+	}
+});
+
+function receiveError(err) {
+	console.info(err);
+}
+
+//Start the server
+server.listen(3000);
---
+++ public/app/app.js
@@ -0,0 +1,32 @@
+var weatherApp = angular.module('weatherApp', []);
+
+weatherApp.controller('WeatherController', function WeatherController($scope) {
+  
+  $scope.ws = new WebSocket("ws://" + window.location.host + "/resources/ws/sensor");
+
+  $scope.ws.onopen = function (ev) {
+    console.log("[ws open]");
+    reconnect = true;
+  }
+
+  $scope.ws.onclose = function (ev) {
+    console.log("[ws close]");
+    if(reconnect) {
+      startWebSocket();
+      reconnect = false;
+    }
+  }
+
+  $scope.ws.onmessage = function (ev) {
+    console.log("[ws message]");
+    var payload = JSON.parse(ev.data);
+    $scope.updateData(payload);
+  }
+
+  $scope.updateData = function (payload) {
+    console.info(payload);
+    $scope.data = payload;
+    $scope.$apply();
+  }
+
+});
